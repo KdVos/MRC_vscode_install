4351cb0d3d20c70131c10e500f25f10f7b901075
Koen de Vos <50488480+KdVos@users.noreply.github.com>
2022-09-27 13:11:17 +0200

    Add files via upload

---
+++ vscode_install.bash
@@ -0,0 +1,15 @@
+sudo snap install --classic code
+
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension jeff-hykin.better-cpp-syntax
+code --install-extension twxs.cmake
+code --install-extension josetr.cmake-language-support-vscode
+code --install-extension ms-vscode.cmake-tools
+
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+
+code --install-extension gruntfuggly.todo-tree
+
+code --install-extension ms-iot.vscode-ros
