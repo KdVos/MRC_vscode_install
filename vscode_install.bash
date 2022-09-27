8163b9a6f9f1ec1023691e9005ec87956b30f90d
Koen de Vos <50488480+KdVos@users.noreply.github.com>
2022-09-27 13:17:24 +0200

    Update vscode_install.bash

--- vscode_install.bash
+++ vscode_install.bash
@@ -1,3 +1,4 @@
+{
 sudo snap install --classic code
 
 code --install-extension ms-vscode.cpptools
@@ -13,3 +14,8 @@ code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension gruntfuggly.todo-tree
 
 code --install-extension ms-iot.vscode-ros
+
+cd $HOME/.config/Code/User/
+
+wget -O settings.json https://raw.githubusercontent.com/KdVos/MRC_vscode_install/main/settings.json?token=GHSAT0AAAAAABYEUWM4ZNBDWYBQJWTH27DWYZS3NDA
+}
